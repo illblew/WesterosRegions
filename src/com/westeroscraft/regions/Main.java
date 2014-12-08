706e9124487cf861877394190e313303eb08f6cb
Will Blew <willblew@gmail.com>
2014-12-08 18:36:07 -0500

    Add kickback logic
    Kicks user back if they attempt to enter.

--- src/com/westeroscraft/regions/Main.java
+++ src/com/westeroscraft/regions/Main.java
@@ -2,6 +2,8 @@ package com.westeroscraft.regions;
 //This plugin was written by Will Blew for WesterosCraft in 2014
 //This plugin is based on the concepts of ALL team members.
 
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.PluginManager;
 
@@ -41,11 +43,14 @@ public class Main extends JavaPlugin implements Listener  {
 		
 		if (p.getTotalExperience() < price.floatValue()) 
 		{
-		e.getPlayer().sendMessage("You just entered " + e.getRegion().getId() + " it takes " + price + "XP and you only have " + p.getExp());
+		e.getPlayer().sendMessage(ChatColor.RED + "You just tried to enter " + e.getRegion().getId() + " it takes " + price + "XP and you only have " + ((int)p.getExp()));
+		e.setCancelled(true);
+		Location location = new Location(p.getWorld(),(p.getLocation().getX() - 25),p.getLocation().getY(),p.getLocation().getZ());
+		p.teleport(location);
 		}
 		else {
-			p.setExp(p.getTotalExperience() - price.floatValue());
-			e.getPlayer().sendMessage("You just entered " + e.getRegion().getId() + " it cost you " + price);
+			p.giveExp((int) -price);
+			e.getPlayer().sendMessage(ChatColor.GREEN + "You just entered " + e.getRegion().getId() + " it cost you " + price);
 		}
 	}
 }
