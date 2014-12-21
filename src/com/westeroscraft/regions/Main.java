2487ee4534d5697e5c055960d8f03f66cb4f7f38
Will Blew <willblew@gmail.com>
2014-12-20 19:21:52 -0500

    Adds playercheck and update

--- src/com/westeroscraft/regions/Main.java
+++ src/com/westeroscraft/regions/Main.java
@@ -22,6 +22,10 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
 public class Main extends JavaPlugin implements Listener  {
@@ -47,19 +51,47 @@ public class Main extends JavaPlugin implements Listener  {
 	}
 	
 	@EventHandler
-	public void onRegionEnter(RegionEnterEvent e)
+	public void onPlayerJoin(PlayerJoinEvent event) throws Exception {
+		//add to players if not found
+		Connection con;
+		con = cSQL.conJoner();
+		String p = event.getPlayer().getName().toString();
+		String csql = ("SELECT COUNT(name) as rowcount FROM Players WHERE name ='" + p + "'");
+		statement = con.createStatement();
+		ResultSet results = statement.executeQuery(csql);
+		results.next();
+		int count = results.getInt("rowcount");
+		results.close();
+		log("Players Found: " + count);
+		if (count == 0) {
+			//Date
+	    	Calendar cal = Calendar.getInstance();
+	    	SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+			//construct
+			String usql = ("INSERT INTO Players VALUES (0,'" + p + "','none',0,Now())");
+			//create them
+			
+			statement.executeUpdate(usql);
+		}
+			
+	}
+	
+	@EventHandler
+	public void onRegionEnter(RegionEnterEvent e) throws SQLException
 	{
 		Player p = e.getPlayer();
 		Double price = e.getRegion().getFlag(DefaultFlag.PRICE);
+		String Region = e.getRegion().getId();
+		Connection con;
 		
 		if (p.getTotalExperience() < price.floatValue()) 
 		{
-		// Say no.
-		e.getPlayer().sendMessage(ChatColor.RED + "You just tried to enter " + e.getRegion().getId() + " it takes " + price + "XP and you only have " + ((int)p.getExp()));
+		// Say no
+		e.getPlayer().sendMessage(ChatColor.RED + "You just tried to enter " + Region + " it takes " + price + "XP and you only have " + ((int)p.getExp()));
 		// Kill it with fire
 		e.setCancelled(true);
 		// Set new location with smallest X as possible.
-		Location location = new Location(p.getWorld(),(p.getLocation().getX() - 10),p.getLocation().getY(),p.getLocation().getZ());
+		Location location = new Location(p.getWorld(),(p.getLocation().getX() - 20),p.getLocation().getY(),p.getLocation().getZ());
 		p.teleport(location); //do it
 		
 		
@@ -67,12 +99,24 @@ public class Main extends JavaPlugin implements Listener  {
 		}
 		else {
 			p.giveExp((int) -price);
-			e.getPlayer().sendMessage(ChatColor.GREEN + "You just entered " + e.getRegion().getId() + " it cost you " + price);
+			e.getPlayer().sendMessage(ChatColor.GREEN + "You just entered " + Region + " it cost you " + price);
 			
 			// Location logic goes here
-			
-				// Aka check type of area
-			
+			//get region info
+			String regSql = ("SELECT * FROM Regions WHERE name ='" + Region + "'");
+			try {
+				con = cSQL.conJoner();
+				statement = con.createStatement();
+				ResultSet results = statement.executeQuery(regSql);
+				results.next();
+				String regId = results.getString("id");
+				String regLock = results.getString("locked");
+				String regactions = results.getString("actions");
+				results.close();
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				log(e1.toString());
+			}
 			// Spawn instance
 			
 				// Check minimum players for instance
